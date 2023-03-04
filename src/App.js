d9cb79cb01abea02d8646cde2e5e21d43c80c1d1
herocodecamp <herocodecamp@gmail.com>
2023-03-04 09:01:42 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import Navbar from "./common/Navbar";
 import Categories from "./components/Categories";
 import Header from "./components/Header";
+import AboutFee from "./components/AboutFee";
+import OurFounder from "./components/OurFounder";
+import Feedback from "./components/Feedback";
+import SelflanceApp from "./components/SelflanceApp";
 import LandingPage from "./pages/LandingPage";
 import Blogs from "./pages/Blogs/Blogs";
 import Footer from "./components/Footer/Footer";
+import WheSelflance from "./components/WheSelflance";
+import FeaturedJobs from "./components/FeaturedJobs";
 import SingleBlog from "./pages/SingleBlog";
+import FAQ from "./common/FAQ";
 
 function App() {
   return (
     <>
    <Navbar/>
-   <SingleBlog />
-   <Footer />
+   {/* <Blogs/> */}
+  <Header/>
+  <Categories/>
+  <WheSelflance/>
+  <FeaturedJobs/>
+  <AboutFee/>
+  <OurFounder/>
+  <Feedback/>
+  <SelflanceApp/>
+  <FAQ/>
+  <Footer/>
    </>
   );
 }
