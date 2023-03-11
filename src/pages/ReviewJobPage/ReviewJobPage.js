0795f5430068c38917adc71e454e946b9c345a44
Hector <hector.herocodecamp@gmail.com>
2023-03-11 16:08:15 +0100

    added review job page

---
+++ src/pages/ReviewJobPage/ReviewJobPage.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../../common/Navbar'
+import Footer from '../../components/Footer/Footer'
+import ReviewJobView from './ReviewJobView'
+
+const ReviewJob = () => {
+  return (
+    <>
+        <Navbar/>
+        <ReviewJobView/>
+        <Footer/>
+    </>
+  )
+}
+
+export default ReviewJob
