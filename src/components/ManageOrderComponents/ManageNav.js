a2c549832ad9e2322fe5ed8b7d39e62736b3e228
jasim-herocodecamp <jasim.herocodecamp@gmail.com>
2023-03-13 21:01:06 +0600

    manage order page drawer added

--- src/components/ManageOrderComponents/ManageNav.js
+++ src/components/ManageOrderComponents/ManageNav.js
@@ -22,7 +22,7 @@ const ManageNav = () => {
 
   return (
     <div>
-      <div className='bg-[#F4F4F4] flex items-center space-x-4 mt-12 h-black] rounded'>
+      <div className='bg-[#F4F4F4] sm:flex items-center space-x-4 mt-12 h-black] rounded hidden sm:block'>
 
         <button className={getButtonClass(1)} onClick={() => handleButtonClick(1)}>
           Active Orders <sup className='w-4 h-4 mt-[-5px] rounded-full bg-white text-black '>10</sup>
