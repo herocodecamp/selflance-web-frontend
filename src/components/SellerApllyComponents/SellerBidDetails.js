a7ee19b85b74ba2c84af0d0d8fcd8c9c0ec6ec46
jasim-herocodecamp <jasim.herocodecamp@gmail.com>
2023-03-19 18:41:45 +0600

    apply page done

--- src/components/SellerApllyComponents/SellerBidDetails.js
+++ src/components/SellerApllyComponents/SellerBidDetails.js
@@ -1,8 +1,16 @@
 import { Label, TextInput } from 'flowbite-react';
 import React, { useState } from 'react';
+import { AiOutlineFileProtect } from 'react-icons/ai';
+import { BiUpload } from 'react-icons/bi';
 
 const SellerBidDetails = () => {
   const [selectedOption, setSelectedOption] = useState("");
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
 
   const handleChange = (event) => {
     setSelectedOption(event.target.value);
@@ -10,15 +18,15 @@ const SellerBidDetails = () => {
 
   return (
     <div className='py-6'>
-      <div className='space-y-4'>
+      <div className='space-y-4 sm:space-y-8'>
         <h2 className='text-[#15151E] text-xl font-semibold'>Project Bid</h2>
 
-        <div className='bg-[#FFFFFF] drop-shadow-md p-4 space-y-3 rounded-2xl'>
+        <div className='bg-[#FFFFFF] drop-shadow-2xl p-4 space-y-3 rounded-2xl'>
           <div className='flex space-x-4  items-center'>
             <input type="radio" />
             <h3 className='text-[#121F3E]  text-base sm:text-xl'>By Project</h3>
           </div>
-          <p className='text-[#8E8E8E] font-sm'>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour...Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
+          <p className='text-[#8E8E8E] font-sm pr-6'>Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour...Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour</p>
         </div>
 
         <div>
@@ -89,9 +97,60 @@ const SellerBidDetails = () => {
                 </div>
               </div>
 
+              <div className='space-y-4'>
+                <label className='text-[#15151E] text-base font-bold'>Cover Letter</label>
+                <textarea
+                  className='w-11/12 sm:w-full resize-none text-[#8E8E8E] border-2 border-[#E3E3E3] px-4 py-2 pr-8 rounded  shadow leading-tight'
+                  name="description" cols="30" rows="10" placeholder='write your cover letter...'></textarea>
+              </div>
+
+              <div className='space-y-4'>
+                <h4 className='text-[#15151E] text-xl font-bold'>Add files <span className='text-[#8E8E8E]'>(Optional)</span></h4>
+                <h4 className='text-[#15151E] text-base font-bold'>Uplaod file,photo and document</h4>
+
+                <div class="relative w-11/12 sm:w-full resize-none text-[#8E8E8E] border-2 border-[#E3E3E3] px-4 py-2 pr-8 rounded  shadow leading-tight ">
+                  <input
+                    type="file"
+
+                    class="absolute inset-0 w-full h-full opacity-0 cursor-pointer"
+                    onChange={handleFileSelect}
+                  />
+                  <div className='flex justify-between items-center'>
+                    <p>upload file</p>
+                    <span><BiUpload /></span>
+                  </div>
+
+                </div>
+
+              </div>
+
             </div>
 
           </form>
+        </div>
+
+        <div className='sm:pt-12 p-4 sm:p-0'>
+          {selectedFile && (
+            <div className='flex items-center space-x-6'>
+              <span className='bg-[#E2EED8] w-[40px] h-[40px] rounded-full'><AiOutlineFileProtect className='text-[#00538F] text-2xl w-1/2 mx-auto mt-2' /></span>
+              <div className='w-full space-y-4'>
+                <p> file uploading: {selectedFile.name}
+                </p>
+
+                <div className='bg-[#69B22A] bg-opacity-20 rounded-lg h-6 w-11/12'>
+                  <div className='bg-[#00538F] w-1/2 rounded-lg h-6'></div>
+                </div>
+
+              </div>
+            </div>
+          )}
+        </div>
+        <div className='text-center sm:pt-6'>
+
+          <button
+            disabled={!selectedFile}
+            className={`font-bold text-base ${selectedFile ? 'bg-[#00538F]' : 'bg-[#80A9C7]'}  text-white sm:px-6 sm:py-2 px-4 py-2  rounded-lg`}
+          >Submit</button>
         </div>
 
       </div>
