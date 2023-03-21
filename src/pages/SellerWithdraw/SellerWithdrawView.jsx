5acec855c8f9f6f9c92ad660dd72feb30f15e958
jasim-herocodecamp <jasim.herocodecamp@gmail.com>
2023-03-21 11:48:20 +0600

    seller withdraw history section done

--- src/pages/SellerWithdraw/SellerWithdrawView.jsx
+++ src/pages/SellerWithdraw/SellerWithdrawView.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import SeWithdrawHistory from '../../components/SeWithdrawComponent/SeWithdrawHistory';
 import SeWithdrawTopbar from '../../components/SeWithdrawComponent/SeWithdrawTopbar';
 
 const SellerWithdrawView = () => {
   return (
     <div className='sm:py-8 py-4 sm:px-12 px-4'>
       <SeWithdrawTopbar />
+      <SeWithdrawHistory />
     </div>
   );
 };
